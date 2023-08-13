1bc7351e576ed588e5758e0c324d3ec429e12a9e
Asker <asker.kudaev@gmail.com>
2023-08-13 23:28:52 +0700

    created simple backend and authorization pages

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Authorization from "./pages/authorization/Authorization"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Authorization/>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/feed/Feed.jsx
@@ -0,0 +1,12 @@
+
+
+const Feed = () =>{
+
+    return(
+        <div className="constent">
+            <div>
+                <h1>hello</h1>
+            </div>
+        </div>
+    )
+}
